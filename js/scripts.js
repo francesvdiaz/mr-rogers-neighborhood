7d78c26638a1b987d9e4e574974fd474049cfd12
FrancesDiaz <115190000+francesvdiaz@users.noreply.github.com>
2023-02-17 11:54:13 -0800

    pass first TDD test, loop counts up to user number and displays to page.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,26 @@
 
-function() {
+function changeNumber(number) {
+  number = document.getElementById("input").value;
+  let result = [];
 
+    for (i=0;i<=number;i++) {
+      result.push(i);
+  };
+  return result;
 }
+
 
 function handleFormSubmission (event) {
   event.preventDefault();
-  
+  number = document.getElementById("input").value;
+  robogerArray = changeNumber(number);
+  const output = document.createElement("p")
+  const body = document.querySelector("body");
+  const div = document.createElement("div");
+  div.setAttribute("id","output")
+  body.append(div);
+  div.append(output);
+  output.innerText = robogerArray;
 };
 
 window.addEventListener("load", function() {
