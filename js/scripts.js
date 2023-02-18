c8d27282c58fccaf11d6cb479c6ff5fa4b42e4bd
FrancesDiaz <115190000+francesvdiaz@users.noreply.github.com>
2023-02-17 19:20:14 -0800

    pass second test, replacing #1 with beep

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 
-function changeNumber(number) {
+function countNumbers(number) {
   number = document.getElementById("input").value;
   let result = [];
 
@@ -9,18 +9,31 @@ function changeNumber(number) {
   return result;
 }
 
+function changeWord(number) {
+  const result = [];
+  for (let i=0;i<number.length;i++) {
+    if (number[i] === 1) {
+      result.push("beep!");
+    } else {
+      result.push(number[i]);
+    }
+  }
+    return result;
+}
+
 
 function handleFormSubmission (event) {
   event.preventDefault();
   number = document.getElementById("input").value;
-  robogerArray = changeNumber(number);
+  robogerArray = countNumbers(number);
+  const beepArray = changeWord(robogerArray);
   const output = document.createElement("p")
   const body = document.querySelector("body");
   const div = document.createElement("div");
   div.setAttribute("id","output")
   body.append(div);
   div.append(output);
-  output.innerText = robogerArray;
+  output.innerText = beepArray;
 };
 
 window.addEventListener("load", function() {
